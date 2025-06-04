dd71a443855c9961a291781af432509f4586bcb6
Amiir25 <amirsadik636@gmail.com>
2025-06-04 04:07:19 +0300

    Add TrendingNow.jsx

---
+++ netflix_clone/src/components/TrendingNow.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import HorizontalLine from "./HorizontalLine";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+
+
+const TrendingNow = () => {
+    return (
+        <>
+            {/*------------ Horizontal Line ------------*/}
+            <HorizontalLine />
+
+            {/*------------ Trending Now ------------*/}
+            <div id="trending-now" className="mb-80 mt-20 mx-6 md:mx-20 lg:mx-32">
+                {/* Title */}
+                <h2 className="text-white text-lg md:text-2xl lg:text-3xl font-medium mb-6">Trending Now</h2>
+                <div id="movie-wrapper" className="border border-whie relative w-full h-40 md:h-56 lg:h-68 flex items-center">
+                    {/* Left button */}
+                    <div className="absolute -left-1 bg-black px-2 h-full flex items-center justify-center z-10">
+                        <button className=" text-white bg-gray-900 px-0.2 py-8 rounded">
+                            <FontAwesomeIcon icon={faChevronLeft} />
+                        </button>
+                    </div>
+                    {/* right button  */}
+                    <div className="absolute -right-1 bg-black px-2 h-full flex items-center justify-center z-10">
+                        <button className="text-white bg-gray-900 px-0.2 py-8 rounded">
+                            <FontAwesomeIcon icon={faChevronRight}/>
+                        </button>
+                    </div>
+                    {/* Movies */}
+                    <div id="movies" className="w-full h-full overflow-x-auto overflow-y-hidden whitespace-nowrap">
+                        <div className="relative inline-block w-24 md:w-38 lg:w-48 h-full mx-1 md:mx-2 lg:mx-4
+                    bg-[url('./img/spy.webp')] bg-cover bg-center bg-no-repeat rounded-lg">
+                            <h1 className="absolute -left-4 top-1/2 transform -translate-y-1/2 text-white text-7xl">1</h1>
+                        </div>
+                        <div className="relative inline-block w-24 md:w-38 lg:w-48 h-full mx-1 md:mx-2 lg:mx-4 
+                    bg-[url('./img/prison-break.webp')] bg-cover bg-center bg-no-repeat rounded-lg">
+                            <h1 className="absolute -left-4 top-1/2 transform -translate-y-1/2 text-white text-7xl">1</h1>
+                        </div>
+                    </div>
+                </div> {/*  movie-wrapper */}
+            </div>{/* trending-now */}
+        </>
+    )
+}
+
+export default TrendingNow;
