d0d0423cb1d8872d0e624af08afc72ea15bfcd20
Amiir25 <amirsadik636@gmail.com>
2025-06-10 20:01:56 +0300

    Add App.jsx

---
+++ netflix_clone/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header';
+import TrendingNow from './components/TrendingNow';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <TrendingNow/>
+    </>
+  )
+}
+
+export default App
