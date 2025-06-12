bcfbea316dd605fb656b89695c6b6b9d9f69d642
Amiir25 <144411155+Amiir25@users.noreply.github.com>
2025-06-12 20:56:21 +0300

    Update Header.jsx

--- netflix_clone/src/components/Header.jsx
+++ netflix_clone/src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
             style={{backgroundImage: "url('/background.png')"}}>
                 <div id="header-wrapper" className="relative flex-1 bg-[rgba(0,0,0,0.8)]">
 
-                    {/* ------------ Navbar ------------ */}
+                    {/* ------------ Navbar ---------- */}
                     <Navbar />
 
                     {/* ------------ Hero ------------ */}
